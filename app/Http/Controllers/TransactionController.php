21716008ce11ceaa5640480fc0af7a752b21af13
Pankeel <pankeel@cognitivo.in>
2018-02-19 15:46:47 +0530

    update

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -8,76 +8,120 @@ use Illuminate\Http\Request;
 class TransactionController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
         //
     }
 
+    public function getTransaction()
+    {
+        $Transaction=Transaction::with('details')
+        ->get();
+        return response()->json($Transaction);
+    }
+
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function create()
     {
         //
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(Request $request)
     {
-        //
+        if ($request->id==0) {
+            $Transaction= new Transaction();
+
+        }
+        else {
+            $Transaction= Transaction::where('id',$request->id)->first();
+
+        }
+        $Transaction->customer_id=$request->customer_id;
+        $Transaction->supplier_id=$request->supplier_id;
+        $Transaction->document_id=$request->document_id;
+        $Transaction->currency_id=$request->currency_id;
+        $Transaction->rate=$request->rate;
+        $Transaction->payment_condition=$request->payment_condition;
+        $Transaction->chart_account_id=$request->chart_account_id;
+        $Transaction->date=$request->date;
+        $Transaction->number=$request->number;
+        $Transaction->code=$request->code;
+        $Transaction->code_expiry=$request->code_expiry;
+        $Transaction->comment=$request->comment;
+        $Transaction->ref_id=$request->ref_id;
+        $Transaction->save();
+        foreach ($request->details as $detail) {
+            if ($request->id==0) {
+                $TransactionDetail= new TransactionDetail();
+
+            }
+            else {
+                $TransactionDetail= TransactionDetail::where('id',$request->id)->first();
+
+            }
+            $TransactionDetail->transaction_id=$detail->transaction_id();
+            $TransactionDetail->chart_id=$detail->chart_id;
+            $TransactionDetail->chart_value_id=$detail->chart_value_id;
+            $TransactionDetail->value=$detail->value;
+            $TransactionDetail->save();
+
+        }
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
+    * Display the specified resource.
+    *
+    * @param  \App\Transaction  $transaction
+    * @return \Illuminate\Http\Response
+    */
     public function show(Transaction $transaction)
     {
         //
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for editing the specified resource.
+    *
+    * @param  \App\Transaction  $transaction
+    * @return \Illuminate\Http\Response
+    */
     public function edit(Transaction $transaction)
     {
         //
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Transaction  $transaction
+    * @return \Illuminate\Http\Response
+    */
     public function update(Request $request, Transaction $transaction)
     {
         //
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Transaction  $transaction
-     * @return \Illuminate\Http\Response
-     */
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\Transaction  $transaction
+    * @return \Illuminate\Http\Response
+    */
     public function destroy(Transaction $transaction)
     {
         //
