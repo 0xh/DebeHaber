b6db2616387232cb05a39c24784251461b2af42a
abhi-cognitivo <abhi@cognitivo.in>
2018-02-24 21:12:24 -0300

    removed excess dashboard html tags

--- resources/views/vendor/spark/layouts/dashboard.blade.php
+++ resources/views/vendor/spark/layouts/dashboard.blade.php
@@ -42,31 +42,6 @@
                         </ul>
                     @endif
                 </div>
-                <div>
-                    @if(request()->route('cycle') != null)
-                        <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push" data-dropdown-toggle="hover" aria-expanded="true">
-                            <a href="#" class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
-                                <i> {{ request()->route('cycle')->year }} </i>
-                            </a>
-                            <div class="m-dropdown__wrapper">
-                                <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
-                                <div class="m-dropdown__inner">
-                                    <div class="m-dropdown__body">
-                                        <div class="m-dropdown__content">
-                                            <ul class="m-nav">
-                                                <li class="m-nav__section m-nav__section--first m--hide">
-                                                    <span class="m-nav__section-text">
-                                                        Quick Actions
-                                                    </span>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-                </div>
             </div>
         </div>
         <!-- END: Subheader -->
