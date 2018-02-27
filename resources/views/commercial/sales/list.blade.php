ea739e465b46e1823e8f0f765fa35bd69bb8868c
abhi-cognitivo <abhi@cognitivo.in>
2018-02-27 10:39:30 -0300

    datatable

--- resources/views/commercial/sales/list.blade.php
+++ resources/views/commercial/sales/list.blade.php
@@ -1,9 +1,8 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 {{-- <router-view name="Datatable"  :taxpayer="{{ request()->route('taxPayer')}}"  /> --}}
 
-<sales-list :taxpayer="{{ request()->route('taxPayer')->id}}"  inline-template>
-  <div>
-
- <datatable v-bind="$data" />
-</div>
+<sales-list :taxpayer="{{ request()->route('taxPayer')->id}}" inline-template>
+    <div>
+        <datatable v-bind="$data" />
+    </div>
 </sales-list>
