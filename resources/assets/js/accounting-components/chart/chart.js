793a62d54734775f77ed85f93555e6ffad80bd83
abhi-cognitivo <abhi@cognitivo.in>
2018-03-23 16:26:20 -0300

    update url for https on charts. quick fix. needs to be improved

--- resources/assets/js/accounting-components/chart/chart.js
+++ resources/assets/js/accounting-components/chart/chart.js
@@ -128,7 +128,7 @@ Vue.component('chart',{
 
         var app = this;
         $.ajax({
-            url: '/api/'  + this.taxpayer + '/' + this.cycle +'/accounting/chart/get_charts/' ,
+            url: 'https://www.debehaber.com/api/'  + this.taxpayer + '/' + this.cycle +'/accounting/chart/get_charts/' ,
             headers: {'X-CSRF-TOKEN': CSRF_TOKEN},
             type: 'get',
             dataType: 'json',
