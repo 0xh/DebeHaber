802fe0cfd4dcae3e03405e7171b37d69e1d7fa3e
abhi-cognitivo <abhi@cognitivo.in>
2018-03-24 09:10:21 -0300

    update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -36,7 +36,7 @@ class ReportController extends Controller
         {
             $data = $this->vatPurchaseQuery($taxPayer, $startDate, $endDate);
 
-            return view('reports/PRY/purchase_ByVAT')
+            return view('reports/PRY/purchases_ByVAT')
             ->with('header', $taxPayer)
             ->with('data', $data)
             ->with('strDate', $startDate)
@@ -50,7 +50,7 @@ class ReportController extends Controller
         {
             $data = $this->vatPurchaseQuery($taxPayer, $startDate, $endDate);
 
-            return view('reports/PRY/purchase_ByVAT')
+            return view('reports/PRY/purchases_ByVAT')
             ->with('header', $taxPayer)
             ->with('data', $data)
             ->with('strDate', $startDate)
@@ -64,7 +64,7 @@ class ReportController extends Controller
         {
             $data = $this->vatPurchaseQuery($taxPayer, $startDate, $endDate);
 
-            return view('reports/PRY/purchase_ByVAT')
+            return view('reports/PRY/purchases_ByVAT')
             ->with('header', $taxPayer)
             ->with('data', $data)
             ->with('strDate', $startDate)
