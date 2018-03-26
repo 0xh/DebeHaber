46f67ea0875864eb96290d53444d3aa03b69e880
abhi-cognitivo <abhi@cognitivo.in>
2018-03-26 09:37:05 -0400

    reverted code so accountant can work fixing wrong data./

--- resources/views/commercial/sales/list.blade.php
+++ resources/views/commercial/sales/list.blade.php
@@ -3,27 +3,20 @@
 <sales-list :taxpayer="{{ request()->route('taxPayer')->id}}" :cycle="{{ request()->route('cycle')->id }}" inline-template>
     <div>
         <div>
-            <router-view name="create" :taxpayer="{{ request()->route('taxPayer')->id}}" :cycle="{{ request()->route('cycle')->id }}"></router-view>
+            <router-view name="create" :taxpayer="{{ request()->route('taxPayer')->id}}" :cycle="{{ request()->route('cycle')->id }}" />
         </div>
         <div>
-            <p v-for="item in list">
-                Line:
-                <span v-text="item"></span>
-            </p>
-            <infinite-loading @infinite="infiniteHandler"></infinite-loading>
-        </div>
+            <router-view name="List" :taxpayer="{{ request()->route('taxPayer')->id}}" :cycle="{{ request()->route('cycle')->id }}" inline-template>
+                <div>
+                    <datatable v-bind="$data">
+
+                        {{-- <button class="btn btn-default" @click="alertSelectedUids" :disabled="!selection.length">
+                        <i class="fa fa-task"></i>
+                        Alert selected uid(s)
+                    </button> --}}
+                </datatable>
+            </div>
+        </router-view>
     </div>
-</sales-list>
-
-
-{{-- <router-view name="List" :taxpayer="{{ request()->route('taxPayer')->id}}" :cycle="{{ request()->route('cycle')->id }}" inline-template>
-<div>
-<datatable v-bind="$data">
-
-<button class="btn btn-default" @click="alertSelectedUids" :disabled="!selection.length">
-<i class="fa fa-task"></i>
-Alert selected uid(s)
-</button>
-</datatable>
 </div>
-</router-view> --}}
+</sales-list>
