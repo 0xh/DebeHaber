29f8801021784a0f72e4a9d1748d0e524f81e999
abhi-cognitivo <abhi@cognitivo.in>
2018-03-27 13:46:46 -0400

    update for testing ajax calls.

--- resources/assets/js/accounting-components/chart/chart.js
+++ resources/assets/js/accounting-components/chart/chart.js
@@ -125,10 +125,9 @@ Vue.component('chart',{
         app.coefficient = data.coefficient;
     },
     init(){
-
         var app = this;
         $.ajax({
-            url: 'https://www.debehaber.com/api/'  + this.taxpayer + '/' + this.cycle +'/accounting/chart/get_charts/' ,
+            url: '/api/'  + this.taxpayer + '/' + this.cycle +'/accounting/chart/get_charts/' ,
             headers: {'X-CSRF-TOKEN': CSRF_TOKEN},
             type: 'get',
             dataType: 'json',
