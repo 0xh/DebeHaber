ea8a1e7ca327b750cb9a53c593eeeec0fec428bb
abhi-cognitivo <abhi@cognitivo.in>
2018-04-24 15:07:29 -0400

    fixed softedelete call in inventory. not in database. must fix.

--- app/Inventory.php
+++ app/Inventory.php
@@ -3,14 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\ModelStatus\HasStatuses;
 
 class Inventory extends Model
 {
     //
-    use SoftDeletes, HasStatuses;
+    use HasStatuses;
 
-
-    
 }
