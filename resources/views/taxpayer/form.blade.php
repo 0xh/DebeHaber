3b90886a303edec6af2cb6904226ddcf6d8ecbb7
abhi-cognitivo <abhi@cognitivo.in>
2018-05-02 14:19:15 -0400

    update

--- resources/views/taxpayer/form.blade.php
+++ resources/views/taxpayer/form.blade.php
@@ -29,57 +29,69 @@
                         </div>
                     </div>
 
-                    <div class="m-wizard m-wizard--1 m-wizard--success m-wizard--step-first" id="m_wizard">
+                    <div class="m-wizard m-wizard--2 m-wizard--success m-wizard--step-first" id="m_wizard">
                         <div class="m-portlet__padding-x">
                             <!-- Here you can put a message or alert -->
                         </div>
 
                         <div class="m-wizard__head m-portlet__padding-x">
                             <!--begin: Form Wizard Progress -->
-                            {{-- <div class="m-wizard__progress">
+                            <div class="m-wizard__progress">
                                 <div class="progress">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: calc(50% + 28px);"></div>
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: @{{ progBarPercent }};"></div>
                                 </div>
-                            </div> --}}
+                            </div>
 
                             <div class="m-wizard__nav">
                                 <div class="m-wizard__steps">
                                     <div class="m-wizard__step m-wizard__step--current" data-wizard-target="#m_wizard_form_step_1">
+                                        <a href="#" class="m-wizard__step-number">
+                                            <span>
+                                                <i class="la la-briefcase"></i>
+                                            </span>
+                                        </a>
                                         <div class="m-wizard__step-info">
-                                            <a href="#" class="m-wizard__step-number">
-                                                <span>
-                                                    <span>
-                                                        1
-                                                    </span>
-                                                </span>
-                                            </a>
-                                            <div class="m-wizard__step-line">
-                                                <span></span>
+                                            <div class="m-wizard__step-title">
+                                                1. @lang('global.Taxpayer')
                                             </div>
-                                            <div class="m-wizard__step-label">
-                                                @lang('global.Taxpayer')
+                                            <div class="m-wizard__step-desc">
+                                                All Taxpayer related information
                                             </div>
                                         </div>
                                     </div>
                                     <div class="m-wizard__step" data-wizard-target="#m_wizard_form_step_2">
+                                        <a href="#" class="m-wizard__step-number">
+                                            <span>
+                                                <i class="la la-user"></i>
+                                            </span>
+                                        </a>
                                         <div class="m-wizard__step-info">
-                                            <a href="#" class="m-wizard__step-number">
-                                                <span>
-                                                    <span>
-                                                        2
-                                                    </span>
-                                                </span>
-                                            </a>
-                                            <div class="m-wizard__step-line">
-                                                <span></span>
+                                            <div class="m-wizard__step-title">
+                                                2. @lang('global.ProfileSettings')
                                             </div>
-                                            <div class="m-wizard__step-label">
-                                                @lang('global.ProfileAndSettings')
+                                            <div class="m-wizard__step-desc">
+                                                All accounting information
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="m-wizard__step" data-wizard-target="#m_wizard_form_step_3">
+                                        <a href="#" class="m-wizard__step-number">
+                                            <span>
+                                                <i class="la la-check"></i>
+                                            </span>
+                                        </a>
+                                        <div class="m-wizard__step-info">
+                                            <div class="m-wizard__step-title">
+                                                3. @lang('global.Confirmation')
+                                            </div>
+                                            <div class="m-wizard__step-desc">
+                                                Check data and save.
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <!--end: Form Wizard Nav -->
                         </div>
 
                         <div class="m-wizard__form">
@@ -90,7 +102,7 @@
                                     <!--begin: Form Wizard Step 1-->
                                     <div class="m-wizard__form-step m-wizard__form-step--current" id="m_wizard_form_step_1">
                                         <div class="row">
-                                            <div class="col-xl-8 offset-xl-2">
+                                            <div class="col-xl-10 offset-xl-2">
                                                 <div class="m-form__section m-form__section--first">
                                                     <div class="m-form__heading">
                                                         <h3 class="m-form__heading-title">
@@ -132,6 +144,107 @@
                                                         </div>
                                                     </div>
                                                 </div>
+
+                                                <div class="form-group m-form__group row">
+                                                    <label class="col-lg-2 col-form-label">
+                                                        Account Type
+                                                    </label>
+                                                    <div class="col-lg-10">
+                                                        <div class="row">
+                                                            <div class="col-lg-4">
+                                                                <label class="m-option m-option--plain">
+                                                                    <span class="m-option__control">
+                                                                        <span class="m-radio m-radio--brand">
+                                                                            <input type="radio" name="m_option_1" value="1">
+                                                                            <span></span>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="m-option__label">
+                                                                        <span class="m-option__head">
+                                                                            <span class="m-option__title">
+                                                                                @lang('global.Accountant')
+                                                                            </span>
+                                                                        </span>
+                                                                        <span class="m-option__body">
+                                                                            @lang('global.AccountantDesc')
+                                                                            <hr>
+                                                                            <ul>
+                                                                                <li class="m--font-info">@lang('global.ViewData', ['module' => __('global.Commercial')])</li>
+                                                                                <li class="m--font-info">@lang('global.ManageData', ['module' => __('global.Commercial')])</li>
+                                                                                <li class="m--font-info">@lang('global.ViewData', ['module' => __('accounting.Accounting')])</li>
+                                                                                <li class="m--font-info">@lang('global.ManageData', ['module' => __('accounting.Accounting')])</li>
+                                                                                <li class="m--font-info">@lang('global.Post', ['module' => __('global.Comment')])</li>
+                                                                                <li class="m--font-danger">@lang('global.AuditorPlatform') <small>[@lang('SpecialReport_KPI')]</small></li>
+                                                                            </ul>
+                                                                        </span>
+                                                                    </span>
+
+                                                                </label>
+                                                            </div>
+                                                            <div class="col-lg-4">
+                                                                <label class="m-option m-option--plain">
+                                                                    <span class="m-option__control">
+                                                                        <span class="m-radio m-radio--brand">
+                                                                            <input type="radio" name="m_option_1" value="1">
+                                                                            <span></span>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="m-option__label">
+                                                                        <span class="m-option__head">
+                                                                            <span class="m-option__title">
+                                                                                @lang('global.Personal')
+                                                                            </span>
+                                                                        </span>
+                                                                        <span class="m-option__body">
+                                                                            @lang('global.PersonalDesc')
+                                                                            <hr>
+                                                                            <ul>
+                                                                                <li class="m--font-info">@lang('global.ViewData', ['module' => __('global.Commercial')])</li>
+                                                                                <li class="m--font-info">@lang('global.ManageData', ['module' => __('global.Commercial')])</li>
+                                                                                <li class="m--font-info">@lang('global.ViewData', ['module' => __('accounting.Accounting')])</li>
+                                                                                <li class="m--font-danger">@lang('global.ManageData', ['module' => __('accounting.Accounting')])</li>
+                                                                                <li class="m--font-info">@lang('global.Post', ['module' => __('global.Comment')])</li>
+                                                                                <li class="m--font-danger">@lang('global.AuditorPlatform') <small>[@lang('SpecialReport_KPI')]</small></li>
+                                                                            </ul>
+                                                                        </span>
+                                                                    </span>
+
+                                                                </label>
+                                                            </div>
+                                                            <div class="col-lg-4">
+                                                                <label class="m-option m-option--plain">
+                                                                    <span class="m-option__control">
+                                                                        <span class="m-radio m-radio--brand">
+                                                                            <input type="radio" name="m_option_1" value="1">
+                                                                            <span></span>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="m-option__label">
+                                                                        <span class="m-option__head">
+                                                                            <span class="m-option__title">
+                                                                                @lang('global.Auditor')
+                                                                            </span>
+                                                                        </span>
+                                                                        <span class="m-option__body">
+                                                                            @lang('global.AuditorDesc')
+                                                                            <hr>
+                                                                            <ul>
+                                                                                <li class="m--font-info">@lang('global.ViewData', ['module' => __('global.Commercial')])</li>
+                                                                                <li class="m--font-danger">@lang('global.ManageData', ['module' => __('global.Commercial')])</li>
+                                                                                <li class="m--font-info">@lang('global.ViewData', ['module' => __('accounting.Accounting')])</li>
+                                                                                <li class="m--font-danger">@lang('global.ManageData', ['module' => __('accounting.Accounting')])</li>
+                                                                                <li class="m--font-info">@lang('global.Post', ['module' => __('global.Comment')])</li>
+                                                                                <li class="m--font-info">@lang('global.AuditorPlatform') <small>[@lang('SpecialReport_KPI')]</small></li>
+                                                                            </ul>
+                                                                        </span>
+                                                                    </span>
+
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
                                                 <div class="m-separator m-separator--dashed m-separator--lg"></div>
                                                 <div class="m-form__section">
                                                     <div class="m-form__heading">
