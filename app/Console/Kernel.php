070ff1a232d5f6bf9243c234d2ac19a896003091
abhi-cognitivo <abhi@cognitivo.in>
2018-07-22 07:33:50 -0400

    test2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,9 +25,9 @@ class Kernel extends ConsoleKernel
     */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('currency:rates')
-        ->hourly()
-        ->between('7:00', '12:00');
+        $schedule->command('currency:rates')->hourly();
+        // ->hourly()
+        // ->between('7:00', '12:00');
     }
 
     /**
