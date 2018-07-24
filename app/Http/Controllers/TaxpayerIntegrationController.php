bb9b9acec3958cbfa77602d411edbc02cf780212
pankeel <pankeel@cognitivo.in>
2018-07-24 18:22:17 +0530

    update

--- app/Http/Controllers/TaxpayerIntegrationController.php
+++ app/Http/Controllers/TaxpayerIntegrationController.php
@@ -9,113 +9,122 @@ use Illuminate\Http\Request;
 
 class TaxpayerIntegrationController extends Controller
 {
-    /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function index($teamID, $userID)
-    {
-        $taxPayerIntegration = TaxpayerIntegration::MyTaxPayers($teamID)
-        ->leftJoin('taxpayer_favs', 'taxpayer_favs.taxpayer_id', 'taxpayers.id')
-        ->select('taxpayer_integrations.id as id',
-        'taxpayers.country',
-        'taxpayers.name',
-        'taxpayers.alias',
-        'taxpayers.taxid',
-        'taxpayer_favs.id as is_favorite')
-        ->get();
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index($teamID, $userID)
+  {
+    $taxPayerIntegration = TaxpayerIntegration::MyTaxPayers($teamID)
+    ->leftJoin('taxpayer_favs', 'taxpayer_favs.taxpayer_id', 'taxpayers.id')
+    ->select('taxpayer_integrations.id as id',
+    'taxpayers.country',
+    'taxpayers.name',
+    'taxpayers.alias',
+    'taxpayers.taxid',
+    'taxpayer_favs.id as is_favorite')
+    ->get();
 
-        return $taxPayerIntegration;
+    return $taxPayerIntegration;
+  }
+
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+    //
+  }
+
+  /**
+  * Display the specified resource.
+  *
+  * @param  \App\TaxpayerIntegration  $taxpayerIntegration
+  * @return \Illuminate\Http\Response
+  */
+  public function show($taxpayerIntegrationID)
+  {
+    $taxPayerIntegration = TaxpayerIntegration::where('id', $taxpayerIntegrationID)
+    ->with(['taxpayer', 'taxpayer.setting'])
+    ->get();
+
+    return view('taxpayer/profile')->with('taxPayerIntegration', $taxPayerIntegration);
+  }
+
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  \App\TaxpayerIntegration  $taxpayerIntegration
+  * @return \Illuminate\Http\Response
+  */
+  public function edit(TaxpayerIntegration $taxpayerIntegration)
+  {
+    //
+  }
+
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \App\TaxpayerIntegration  $taxpayerIntegration
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, TaxpayerIntegration $taxPayerIntegration)
+  {
+    if (isset($taxPayerIntegration))
+    {
+      $taxPayer = TaxPayer::where('id', $taxPayerIntegration->taxpayer_id)->with('setting')->first();
+
+      if (isset($taxPayer))
+      {
+        $taxPayer->alias = $request->alias;
+        $taxPayer->address = $request->address;
+        $taxPayer->telephone = $request->telephone;
+        $taxPayer->email = $request->email;
+        $taxPayer->save();
+
+
+        $taxPayer->$setting->update(['regime_type' => $request->setting_regime
+        ,'agent_name' => $request->setting_agent
+        ,'agent_taxid' => $request->setting_agenttaxid
+        ,'show_inventory' => $request->setting_inventory = true ? 1 : 0
+        ,'show_production' => $request->setting_production = true ? 1 : 0
+        ,'show_fixedasset' => $request->setting_fixedasset = true ? 1 : 0
+        ,'does_export' =>  $request->setting_export = true ? 1 : 0
+        ,'does_import' =>$request->setting_import = true ? 1 : 0
+        ,'is_company' => $request->setting_is_company
+      ]);
+
+      // $setting = TaxpayerSetting::where('taxpayer_id', $taxPayer->id)->first() ?? new TaxpayerSetting();
+      // $setting->regime_type = $request->setting_regime;
+      // $setting->agent_name = $request->setting_agent;
+      // $setting->agent_taxid = $request->setting_agenttaxid ;
+      // $setting->show_inventory = $request->setting_inventory = true ? 1 : 0;
+      // $setting->show_production = $request->setting_production = true ? 1 : 0;
+      // $setting->show_fixedasset = $request->setting_fixedasset = true ? 1 : 0;
+      // $setting->does_export = $request->setting_export = true ? 1 : 0;
+      // $setting->does_import = $request->setting_import = true ? 1 : 0;
+      // $setting->is_company = $request->setting_is_company;
+      // $setting->save();
     }
 
-    /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function create()
-    {
-        //
-    }
+    return response()->json('Ok', 200);
+  }
 
-    /**
-    * Display the specified resource.
-    *
-    * @param  \App\TaxpayerIntegration  $taxpayerIntegration
-    * @return \Illuminate\Http\Response
-    */
-    public function show($taxpayerIntegrationID)
-    {
-        $taxPayerIntegration = TaxpayerIntegration::where('id', $taxpayerIntegrationID)
-        ->with(['taxpayer', 'taxpayer.setting'])
-        ->get();
+  return response()->json('Resource not found', 404);
+}
 
-        return view('taxpayer/profile')->with('taxPayerIntegration', $taxPayerIntegration);
-    }
-
-    /**
-    * Show the form for editing the specified resource.
-    *
-    * @param  \App\TaxpayerIntegration  $taxpayerIntegration
-    * @return \Illuminate\Http\Response
-    */
-    public function edit(TaxpayerIntegration $taxpayerIntegration)
-    {
-        //
-    }
-
-    /**
-    * Update the specified resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \App\TaxpayerIntegration  $taxpayerIntegration
-    * @return \Illuminate\Http\Response
-    */
-    public function update(Request $request, TaxpayerIntegration $taxPayerIntegration)
-    {
-        if (isset($taxPayerIntegration))
-        {
-            $taxPayer = TaxPayer::where('id', $taxPayerIntegration->taxpayer_id)->with('setting')->first();
-
-            if (isset($taxPayer))
-            {
-                $taxPayer->alias = $request->alias;
-                $taxPayer->address = $request->address;
-                $taxPayer->telephone = $request->telephone;
-                $taxPayer->email = $request->email;
-                $taxPayer->save();
-
-
-                $taxPayer->$setting->update(['name' => 'Test']);
-
-                $setting = TaxpayerSetting::where('taxpayer_id', $taxPayer->id)->first() ?? new TaxpayerSetting();
-                $setting->regime_type = $request->setting_regime;
-                $setting->agent_name = $request->setting_agent;
-                $setting->agent_taxid = $request->setting_agenttaxid ;
-                $setting->show_inventory = $request->setting_inventory = true ? 1 : 0;
-                $setting->show_production = $request->setting_production = true ? 1 : 0;
-                $setting->show_fixedasset = $request->setting_fixedasset = true ? 1 : 0;
-                $setting->does_export = $request->setting_export = true ? 1 : 0;
-                $setting->does_import = $request->setting_import = true ? 1 : 0;
-                $setting->is_company = $request->setting_is_company;
-                $setting->save();
-            }
-
-            return response()->json('Ok', 200);
-        }
-
-        return response()->json('Resource not found', 404);
-    }
-
-    /**
-    * Remove the specified resource from storage.
-    *
-    * @param  \App\TaxpayerIntegration  $taxpayerIntegration
-    * @return \Illuminate\Http\Response
-    */
-    public function destroy(TaxpayerIntegration $taxpayerIntegration)
-    {
-        //
-    }
+/**
+* Remove the specified resource from storage.
+*
+* @param  \App\TaxpayerIntegration  $taxpayerIntegration
+* @return \Illuminate\Http\Response
+*/
+public function destroy(TaxpayerIntegration $taxpayerIntegration)
+{
+  //
+}
 }
--- app/TaxpayerSetting.php
+++ app/TaxpayerSetting.php
@@ -6,15 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaxpayerSetting extends Model
 {
-    protected $fillable = [
-        'agent_name',
-        
-    ];
+  protected $fillable = [
+    'taxpayer_id',
+    'type',
+    'regime_type',
+    'agent_name',
+    'agent_taxid',
+    'show_inventory',
+    'show_production',
+    'show_fixedasset',
+    'does_import',
+    'does_export',
+    'is_company'
+  ];
 
-    public $timestamps  = false;
+  public $timestamps  = false;
 
-    public function taxpayer()
-    {
-        return $this->belongsTo('App\Taxpayer', 'taxpayer_id', 'id');
-    }
+  public function taxpayer()
+  {
+    return $this->belongsTo('App\Taxpayer', 'taxpayer_id', 'id');
+  }
 }
