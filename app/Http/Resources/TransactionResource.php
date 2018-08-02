5be2053d2c5991f4e3d4d42c9b632a8d3c895ce1
pankeel <pankeel@cognitivo.in>
2018-08-02 15:57:02 +0530

    update

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -14,19 +14,19 @@ class TransactionResource extends JsonResource
     */
     public function toArray($request)
     {
-        return [
-            'ID' =>$request->resource['id'],
-            'Customer' => $this->resource['taxPayer.name'],
-            'CustomerTaxID' => $this->resource['taxPayer.taxid'],
-            'Currency' => $request->resource['currency.code'],
-            'PaymentCondition' => $request->resource['payment_condition'],
-            'PaymentCondition' => $request->resource['payment_condition'],
-            'Date' => $request->resource['date'],
-            'Number' => $this->resource['number'],
-            'Status' => $this->resource['Status.name'],
-
-            'expiryDate' => $this->resource['expiry_date']
-        ];
-        //return parent::toArray($request);
+        // return [
+        //     'ID' =>$request->resource['id'],
+        //     'Customer' => $this->resource['taxPayer.name'],
+        //     'CustomerTaxID' => $this->resource['taxPayer.taxid'],
+        //     'Currency' => $request->resource['currency.code'],
+        //     'PaymentCondition' => $request->resource['payment_condition'],
+        //     'PaymentCondition' => $request->resource['payment_condition'],
+        //     'Date' => $request->resource['date'],
+        //     'Number' => $this->resource['number'],
+        //     'Status' => $this->resource['Status.name'],
+        //
+        //     'expiryDate' => $this->resource['expiry_date']
+        // ];
+        return parent::toArray($request);
     }
 }
