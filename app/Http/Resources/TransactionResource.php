ded73e5b36efb6def51b86e3198bbbf95256bad6
pankeel <pankeel@cognitivo.in>
2018-08-02 16:38:39 +0530

    update

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -14,19 +14,19 @@ class TransactionResource extends JsonResource
     */
     public function toArray($request)
      {
-        // return [
-        //     'ID' =>$this->resource['id'],
-        //     'Customer' => $this->resource['taxPayer.name'],
-        //     'CustomerTaxID' => $this->resource['taxPayer.taxid'],
-        //     'Currency' => $this->resource['currency.code'],
-        //     'PaymentCondition' => $this->resource['payment_condition'],
-        //     'Date' => $this->resource['date'],
-        //     'Number' => $this->resource['number'],
-        //     'Status' => $this->resource['Status.name'],
-        //     //When Loaded will only bring if "with('details')" is used in eloquent call. Only if eager loaded.
-        //     'details' => $this->resource['details']
-        //
-        // ];
-        return parent::toArray($request);
+        return [
+            'ID' =>$this->resource['id'],
+            'Customer' => $this->resource['tax_payer.name'],
+            'CustomerTaxID' => $this->resource['tax_payer.taxid'],
+            'Currency' => $this->resource['currency.code'],
+            'PaymentCondition' => $this->resource['payment_condition'],
+            'Date' => $this->resource['date'],
+            'Number' => $this->resource['number'],
+            'Status' => $this->resource['Status.name'],
+            //When Loaded will only bring if "with('details')" is used in eloquent call. Only if eager loaded.
+            'details' => $this->resource['details']
+
+        ];
+        //return parent::toArray($request);
     }
 }
